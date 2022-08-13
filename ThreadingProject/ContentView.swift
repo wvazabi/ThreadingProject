0c62fac4220ea2d7130648dabaa8ae8169c4912f
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-13 18:58:57 +0300

    Initial Commit

---
+++ ThreadingProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThreadingProject
+//
+//  Created by Enes Kaya on 13.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThreadingProject/ThreadingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadingProjectApp.swift
+//  ThreadingProject
+//
+//  Created by Enes Kaya on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThreadingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
